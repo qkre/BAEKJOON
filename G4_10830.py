e4eea789e8e1848fae481179cbff8f9c8f70b758
sarumani <qkre2018@gmail.com>
2023-11-13 15:40:48 +0900

    10830 - 행렬 곱셈

--- G4_10830.py
+++ G4_10830.py
@@ -6,14 +6,18 @@ arr = list(list(map(int, input().split())) for _ in range(N))
 
 now = copy.deepcopy(arr)
 
+def power(A, B):
+    N = len(A)
+
+
 for _ in range(B):
     temp = list([1] * N for _ in range(N))
     for i in range(N):
         for j in range(N):
             c = 0
             for k in range(N):
-                c += now[i][k]*arr[k][j] % 1000
-            temp[i][j] = c
+                c += now[i][k]*arr[k][j]
+            temp[i][j] = c % 1000
     now = temp
 
 print(now)
