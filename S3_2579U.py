b4894f30c68811c3720c24fd731837299d390118
sarumani <qkre2018@gmail.com>
2023-11-16 15:34:14 +0900

    2579 - 계단 오르기

--- S3_2579U.py
+++ S3_2579U.py
@@ -6,8 +6,12 @@ N = int(input())
 
 arr = list(int(input()) for _ in range(N))
 
-dp = [0] * N
+dp = [[0, 0, 0] for _ in range(N+1)]
 
-for i in range(N-1):
-    dp[i] =max(dp[i-1] + arr[i], dp[i-1] + arr[i+1])
-     
+for i in range(1, N+1):
+
+    dp[i][0] = max(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = dp[i-1][0] + arr[i-1]
+    dp[i][2] = dp[i-1][1] + arr[i-1]
+
+print(max(dp[-1][1], dp[-1][2]))
